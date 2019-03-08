36e798fa9059d99f2af7684087a1c79b6d6988b0
giodrn91 <gdcruz1991@gmail.com>
2019-03-07 23:23:46 -0600

    primer commit

---
+++ src/cuadromagico/CuadroMagico.java
@@ -0,0 +1,12 @@
+package cuadromagico;
+import java.util.Scanner;
+public class CuadroMagico {
+    static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int n=0;        
+        System.out.print("Digite el numero de la matriz: ");
+        n=sc.nextInt();
+        System.out.println("El numero es: "+n);        
+    }
+    
+}
